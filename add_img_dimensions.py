d708fa77d430adc54384ae6675a58760876b3dc2
radardeprecos <2.89044579e+08+radardeprecos@users.noreply.github.com>
2026-05-31 07:53:29 +0000

    fix: Correção definitiva de layout shift (CLS) e tamanho das imagens

--- add_img_dimensions.py
+++ add_img_dimensions.py
@@ -3,23 +3,41 @@ import os
 import re
 from bs4 import BeautifulSoup
 
-def add_img_dimensions_in_html_and_js(file_path):
+def add_img_dimensions_robust(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         content = f.read()
 
     modified = False
 
+    # Função auxiliar para processar tags <img>
+    def process_img_tag(img_tag_str):
+        nonlocal modified
+        # Usar BeautifulSoup para parsear o fragmento da tag <img>
+        img_soup = BeautifulSoup(img_tag_str, 'html.parser')
+        img = img_soup.find('img')
+        if img:
+            if img.get('width') != '120' or img.get('height') != '120':
+                img['width'] = '120'
+                img['height'] = '120'
+                modified = True
+            
+            new_style = 'width:100%;height:auto;'
+            current_style = img.get('style', '')
+            if new_style not in current_style:
+                if current_style:
+                    img['style'] = current_style + ';' + new_style
+                else:
+                    img['style'] = new_style
+                modified = True
+        return str(img_soup)
+
     # 1. Processar tags <img> diretas no HTML
     soup = BeautifulSoup(content, 'html.parser')
     for img in soup.find_all('img'):
-        if img.get('width') != '120' or img.get('height') != '120':
-            img['width'] = '120'
-            img['height'] = '120'
-            modified = True
-
-        new_style = 'width:100%;height:auto;'
-        if img.get('style') != new_style:
-            img['style'] = new_style
+        original_img_tag = str(img)
+        processed_img_tag = process_img_tag(original_img_tag)
+        if original_img_tag != processed_img_tag:
+            img.replace_with(BeautifulSoup(processed_img_tag, 'html.parser'))
             modified = True
     
     if modified:
@@ -28,27 +46,27 @@ def add_img_dimensions_in_html_and_js(file_path):
     # 2. Processar tags <img> dentro de template literals JavaScript
     # Regex para encontrar template literals que contêm <img> tags
     # Isso é uma simplificação e pode não cobrir todos os casos complexos de JS
+    # A regex busca por `...<img...>...`
     template_literal_pattern = re.compile(r'`([^`]*<img[^>]*>[^`]*)`')
 
     def replace_img_in_template_literal(match):
         nonlocal modified
         literal_content = match.group(1)
-        temp_soup = BeautifulSoup(literal_content, 'html.parser')
-        temp_modified = False
-        for img in temp_soup.find_all('img'):
-            if img.get('width') != '120' or img.get('height') != '120':
-                img['width'] = '120'
-                img['height'] = '120'
-                temp_modified = True
-            
-            new_style = 'width:100%;height:auto;'
-            if img.get('style') != new_style:
-                img['style'] = new_style
-                temp_modified = True
+        # Regex para encontrar tags <img> dentro do conteúdo do template literal
+        img_in_literal_pattern = re.compile(r'<img[^>]*>')
+
+        def process_inner_img(inner_match):
+            nonlocal modified
+            original_inner_img_tag = inner_match.group(0)
+            processed_inner_img_tag = process_img_tag(original_inner_img_tag)
+            if original_inner_img_tag != processed_inner_img_tag:
+                modified = True
+            return processed_inner_img_tag
+
+        new_literal_content = img_in_literal_pattern.sub(process_inner_img, literal_content)
         
-        if temp_modified:
-            modified = True
-            return f'`{str(temp_soup)}`'
+        if new_literal_content != literal_content:
+            return f'`{new_literal_content}`'
         return match.group(0)
 
     new_content = template_literal_pattern.sub(replace_img_in_template_literal, content)
@@ -65,4 +83,4 @@ if __name__ == '__main__':
         files = f.read().splitlines()
 
     for file in files:
-        add_img_dimensions_in_html_and_js(file)
+        add_img_dimensions_robust(file)
